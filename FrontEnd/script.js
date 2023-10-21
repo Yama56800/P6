4a8a07ca57c0ae36116db37ff736cf353af68e4d
Yama56800 <thomawmahiaw@gmail.com>
2023-10-21 15:14:22 +0200

    last commit

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,7 +1,5 @@
 const api = 'http://localhost:5678/api/works';
-let data; // Variable pour stocker les données
-
-
+let data; 
 
 function updateGallery(items) {
     const gallery = document.querySelector(".gallery");
@@ -18,11 +16,7 @@ function updateGallery(items) {
 
         container.appendChild(img);
         container.appendChild(name);
-
-        gallery.appendChild(container);
-
-
-        
+        gallery.appendChild(container); 
     });
 }
 
@@ -30,75 +24,42 @@ fetch(api)
     .then(response => response.json())
     .then(dataResponse => {
         data = dataResponse;
-
-
         updateGallery(data);
         updateGallerymodal(data);
-
-
-        console.log(data);
     });
+
     function fetchData() {
         fetch(api)
         .then(response => response.json())
         .then(dataResponse => {
         data = dataResponse;
-
-
         updateGallery(data);
         updateGallerymodal(data);
-
-
-        console.log(data); 
-    });
+        });
       }
       
-      
-    //   setInterval(fetchData, 5000);
-
-//version modal
-
-
-// fetch(api)
-//     .then(response => response.json())
-//     .then(dataResponse => {
-//         data = dataResponse;
-
-
-//         updateGallerymodal(data);
-
-//         // console.log(data);
-//     });
-
 const btnTous = document.getElementById("tous");
 const btnall = document.querySelectorAll(".all-btn button")
 
 document.addEventListener("DOMContentLoaded", function () {
 
- 
-    
     btnTous.style.backgroundColor = "#1D6154";
     btnTous.style.color = "white";
     btnTous.addEventListener("click", function () {
         const imgTous = data.filter(function (work) {
             return work.categoryId === 1 || work.categoryId === 2 || work.categoryId === 3 || work.categoryId === 4;
         });
-
         btnall.forEach(function (button) {
             button.style.backgroundColor = "#FFFEF8";
             button.style.color = "#1D6154";
         });
-
         btnTous.style.backgroundColor = "#1D6154";
         btnTous.style.color = "white";
 
         updateGallery(imgTous);
-
-        console.log(imgTous);
     });
 
 });
-
 
 const btnObjets = document.getElementById("objets");
 
@@ -115,8 +76,6 @@ btnObjets.addEventListener("click", function () {
    
 
     updateGallery(Objets);
-
-    console.log(Objets);
 });
 const btnappartements = document.getElementById("appartements");
 
@@ -133,8 +92,6 @@ btnappartements.addEventListener("click", function () {
    
 
     updateGallery(appartements);
-
-    console.log(appartements);
 });
 const btnhotels = document.getElementById("hotels");
 
@@ -148,17 +105,10 @@ btnhotels.addEventListener("click", function () {
         });
     btnhotels.style.backgroundColor = "#1D6154";
     btnhotels.style.color = "white";
-   
-    
-   
-
+  
     //update Met à jour la galerie avec les éléments filtrés
     updateGallery(hotels);
-
-    console.log(hotels);
 });
-
-
 
 const logoutButton = document.getElementById("logoutButton");
 const loginButton = document.getElementById("loginButton");
@@ -166,25 +116,19 @@ const allbtn = document.querySelector('.all-btn');
 const headerDiv = document.querySelector('.header-div');
 const token = localStorage.getItem("token");
 
-// Créer la div avec l'id "black-banner"
 const blackBanner = document.createElement("div");
 blackBanner.id = "black-banner";
 
-// Créer l'élément <i> avec la classe "fa-regular fa-pen-to-square"
 const iElement = document.createElement("i");
 iElement.className = "fa-regular fa-pen-to-square";
 
-// Ajouter l'icône en premier lieu
 blackBanner.appendChild(iElement);
 
-// Créer le titre <h4> et lui ajouter du texte
 const h4Element = document.createElement("h4");
 h4Element.textContent = "Mode édition";
 
-// Ajouter l'élément <h4> après l'icône
 blackBanner.appendChild(h4Element);
 
-// Ajouter la div au document (par exemple, au body)
 document.body.appendChild(blackBanner);
 
 if (token) {
@@ -254,10 +198,6 @@ const stopPropagation = function (e) {
 document.querySelectorAll('.js-modal').forEach(a => {
     a.addEventListener('click', openmodal)
 })
-
-
-
-
 
 const imageInput = document.getElementById("imageInput");
 const imagePreview = document.getElementById("imagePreview");
@@ -329,8 +269,6 @@ async function closeimg () {
     
 
 }
-
-
 //open second modal
 
 const secondModal = document.getElementById("second-modal");
@@ -341,8 +279,6 @@ function openSecondModal() {
     secondModal.setAttribute('aria-modal', 'true');
     modal.style.display = "none";
 
-    
-    
     document.body.addEventListener('click', closeSecondModalOnClickOutside);
 }
 
@@ -355,6 +291,7 @@ function closeSecondModal() {
     modal.style.display = "none";
     document.body.removeEventListener('click', closeSecondModalOnClickOutside);
     resetimg();
+
     
 }
 
@@ -368,13 +305,15 @@ function closeSecondModalOnClickOutside(event) {
 const ajouterModalButton = document.querySelector('.js-ajouter-modal');
 ajouterModalButton.addEventListener('click', openSecondModal);
 
-const closeSecondModalButton = document.querySelector(".js-modal-close");
+const closeSecondModalButton = document.querySelector(".js-close");
 
-closeSecondModalButton.addEventListener("click", function (e) {
+closeSecondModalButton.addEventListener("click",(e) =>  {
     e.preventDefault();
     closeSecondModal();
     resetimg();
 });
+
+console.log(closeSecondModalButton);
 
 document.querySelectorAll('.js-modal').forEach(a => {
     a.addEventListener('click', openmodal);
@@ -399,6 +338,7 @@ async function updateGallerymodal(items) {
 
     items.forEach(async (work) => {
         const containerModal = document.createElement("div");
+        containerModal.classList.add("div-gallery-modal");
 
         const img = document.createElement("img");
         img.src = work.imageUrl;
@@ -406,13 +346,11 @@ async function updateGallerymodal(items) {
         // Code ci-dessus pour ajouter la galerie dans la modal
 
         const deleteIcon = document.createElement("i");
-        deleteIcon.className = "fa fa-trash";
+        deleteIcon.className = "fa-solid fa-trash-can";
 
         deleteIcon.addEventListener("click", async function (event) {
             event.preventDefault(); 
-            deleteimg(work,items,containerModal);
-
-            
+            deleteimg(work,items,containerModal); 
         });
 
         containerModal.appendChild(img);
@@ -455,57 +393,17 @@ async function deleteimg(work,items,containerModal) {
                 }
             }
         } else {
-            console.error("La suppression a échoué.");
+            alert("La suppression a échoué.");
         }
     } catch (error) {
-        console.error("Une erreur s'est produite :", error);
+        alert("Une erreur s'est produite :", error);
     }
-
-
-
 }
-
-
-
-
-
-     
-
-
-
-
-
-
-
-
-// Note
-//  asynch dans tout les function api
-//  charge de son coté et ne bloque pas l'application, affiche le resultat apres le chargement en arriere plans
-
-
-
-// -gerer le retour  ok
-// -test en dessous
-// -masquer mdp OK
-// -faire le delete OK
-// -ajouter a closemodal reset imageinput ok a peut pret 
-// -form ne valide pas si form non rempli OK
-// -losque admin connecter barre top noir edition ok 
-// -redimenssioné image image input ok 
-// -faire en sorte que c refresh pas la page au Submit (juste fermer la modal) bug
-// -reset image preview bug
-// note
-
-
 const form = document.getElementById("formphoto");
-
-
-
 
 async function addphoto(event,items,work,containerModal) {
     event.preventDefault();
     
-
     // Récupérer les éléments du formulaire
     const imageInput = document.getElementById("imageInput");
     const title = document.getElementById("title");
@@ -517,7 +415,6 @@ async function addphoto(event,items,work,containerModal) {
 
     // Vérifier si le token est présent
     if (!token) {
-        console.error("Token d'authentification non trouvé. Veuillez vous connecter.");
         return;
     }
 
@@ -525,27 +422,19 @@ async function addphoto(event,items,work,containerModal) {
     const selectedFile = imageInput.files[0];
 
     if (selectedFile && selectedFile.size > maxFileSize) {
-        console.error("Le fichier sélectionné dépasse la taille maximale autorisée (4 Mo).");
         alert("Le fichier sélectionné dépasse la taille maximale autorisée (4 Mo).");
         return;
     }
     const formData = new FormData(form);
-    // formData.append("image", imageInput.files[0]);
-    // formData.append("title", title.value);
-    // formData.append("category", category);
 
     formData.set("image", imageInput.files[0]);
     formData.set("title", title.value);
     formData.set("category", category);
 
-    for (const value of formData.values()) {
-        console.log(value);
-      }
    
     const headers = {
         Authorization: `Bearer ${token}`,
     };
-     console.log(formData)
     try {
         
         const response = await fetch('http://localhost:5678/api/works', {
@@ -554,13 +443,10 @@ async function addphoto(event,items,work,containerModal) {
             headers: headers,
             
         });
+              
         
-       
-       
-        console.log(response)
         // Vérifier la réponse (virer le if else,)
         if (response.ok) {
-            console.log("Données envoyées avec succès !");
         
             fetchData();
             const newWork = await response.json();
@@ -572,9 +458,7 @@ async function addphoto(event,items,work,containerModal) {
 
             const deleteIcon = document.createElement("i");
             deleteIcon.className = "fa fa-trash";
-            
-
-            
+           
 // Ajouter l'événement de suppression au bouton
 deleteIcon.addEventListener("click", async function (event) {
     event.preventDefault();
@@ -611,14 +495,11 @@ deleteIcon.addEventListener("click", async function (event) {
                 }
             }
         } else {
-            console.error("La suppression a échoué.");
+            alert("La suppression a échoué.");
         }
     } catch (error) {
-        console.error("Une erreur s'est produite :", error);
+        alert("Une erreur s'est produite :", error);
     }
-
-
-    console.log('supp elements')
 });
 
               
@@ -631,10 +512,6 @@ deleteIcon.addEventListener("click", async function (event) {
 
             newWorkElement.appendChild(newWorkImage);
             newWorkElement.appendChild(newWorkTitle);
-        
-            
-
-           
 
             const liveUpdates = document.querySelector('.gallery');
             liveUpdates.appendChild(newWorkElement);
@@ -646,20 +523,16 @@ deleteIcon.addEventListener("click", async function (event) {
 
             const liveUpdatesModal = document.querySelector('.gallery-modal');
             liveUpdatesModal.appendChild(newWorkElementModal);
-            
-           
-       
-            
-
+    
             closeSecondModal();
         }
          else {
-            console.error("Erreur lors de l'envoi. Statut de réponse :", response.status);
+            alert("Erreur lors de l'envoi. Statut de réponse :", response.status);
             const responseData = await response.json();
-            console.error("Message d'erreur du serveur :", responseData.error);
+            alert("Message d'erreur du serveur :", responseData.error);
         }
     } catch (error) {
-        console.error("Erreur lors de l'envoi des données :", error);
+        alert("Erreur lors de l'envoi des données :", error);
     }
 }
 
@@ -668,35 +541,7 @@ form.addEventListener("submit", addphoto);
 
 
 
-// test ici try catch et aussi if else 
-// try {
-        
-//      await fetch('http://localhost:5678/api/works', {
-//         method: "POST",
-//         body: formData,
-//         headers: headers,
-        
-//     });
-  
-//     console.log(response)
-//     console.log("Données envoyées avec succès !");
-        
-    
-// } catch (error) {
-  
-//         console.error("Erreur lors de l'envoi des données. Statut de réponse :", response.status);
-//         const responseData = await response.json();
-//         console.error("Message d'erreur du serveur :", responseData.error);
-    
-//     console.error("Erreur lors de l'envoi des données :", error);
-// }
 
-
-
-
-
-
-// parenthese au dessus !!!!!!!!!!!!
 
 
 
